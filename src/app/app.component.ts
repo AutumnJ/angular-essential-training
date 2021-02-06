734c7c63d91a8ff0d897ccea39c1aea1ff43ee71
Autumn Jung <ajung@onemedical.com>
2021-02-05 18:24:44 -0600

    render additional component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+// Components are really just a directive with a view
+// The component metadata inherits from the directive metadata
 @Component({
   selector: 'mw-app',
   templateUrl: './app.component.html',
