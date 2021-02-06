495fd324f3637698263765af2882493f13afd38f
Autumn Jung <ajung@onemedical.com>
2021-02-05 18:52:01 -0600

    use @input

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./media-item.component.css']
 })
 export class MediaItemComponent {
+  // This will tell Angular that we want it to support any property bindings placed on instances of the mw-media-item elements where the property name is mediaItem.
+  // Even though Angular supports this name alias, it is a recommended practice to avoid using that approach by default.
+  @Input('mediaItemToWatch') mediaItem;
+
   onDelete() {
     console.log('deleted');
   }
