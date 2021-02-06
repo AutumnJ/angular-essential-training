42bf5962f49076a83febaac2866c05bb737d6520
Autumn Jung <ajung@onemedical.com>
2021-02-05 18:02:50 -0600

    use w3c recommended naming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // The W3C spec states that custom Dom elements should use at least one dash in their names.
+  // Recommendation is that selectors get prefixed for your app or feature.
+  selector: 'mw-app', // app-root => mw-app
   template: '<h1>My App</h1>'
 })
 export class AppComponent {}
