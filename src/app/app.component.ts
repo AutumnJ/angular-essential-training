8f0955abad329ac899c3131dbc12d8687640a44f
Autumn Jung <ajung@onemedical.com>
2021-02-05 18:08:10 -0600

    move markup into html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  template: '<h1>My App</h1>'
+  // markup that is injected into DOM element comes from this
+  // template: '<h1>My App</h1><p>Keeping track of the media I want to watch</p>' use backticks for multi-line
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
