cf53c1f467f388aee7e33ad65d547beef0fb972d
Autumn Jung <ajung@onemedical.com>
2021-02-05 19:02:57 -0600

    subscribing with @output

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
@@ -7,8 +7,14 @@ import { Component, Input } from '@angular/core';
 })
 export class MediaItemComponent {
   @Input() mediaItem;
+  // What we are doing here is exposing an event that can be subscribed to on our custom component, just like native DOM events.
+  @Output() delete = new EventEmitter();
 
   onDelete() {
     console.log('deleted');
+    // This method expects to be called with an argument that represents data we can send back.
+    // If we didn't have anything to send back for this event, we could pass this emit call null,
+    // but it might be helpful to have our delete event return back what was requested to delete.
+    this.delete.emit(this.mediaItem);
   }
 }
